b0a264297b668a89f87a3389ba1730d5c77aeb45
Erick Alexandre <erick@fatcatdevstudios.cloud>
2025-05-13 23:49:16 -0300

    Site

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { GiPlasticDuck } from "react-icons/gi";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen bg-[#2b889b]">
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <h1 className="text-4xl font-bold">Bem-Vindo</h1>
+          <GiPlasticDuck className="w-41 h-41 text-amber-300" />
+          <p>Site de teste. Hospedagem no ar!!</p>
+        </div>
+      </div>
+    </main>
+  );
+}
